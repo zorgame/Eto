b9ca154dde6e1cd99f0a277c29fc4d996bf9f376
zorgame <46399300+zorgame@users.noreply.github.com>
2020-07-16 00:37:40 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
-/* Hola */
-
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-	res.send('Hello Express app');
-});
-
-app.listen(3000, () => console.log('server started'));
-
 const { Client, Collection } = require('discord.js');
 const Discord = require('discord.js');
 const { token } = require('./config.json');
 const client = new Client();
 
-['alias', 'commands'].forEach(x => (client[x] = new Collection())); //Colocamos nuevas colecciones al Cliente
-['comandos', 'eventos'].forEach(x => require(`./handler/${x}`)(client)); //Hacemos un array con las carpetas que tendrá el handler NO TOCAR.
+['alias', 'commands'].forEach(x => (client[x] = new Collection()));
+['comandos', 'eventos'].forEach(x => require(`./handler/${x}`)(client)); 
 
 client.login(token);
